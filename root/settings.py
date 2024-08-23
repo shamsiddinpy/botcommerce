86b4312b1adbeefc1d9b241fa396a345ce6dbb2a
shamsiddindaminov <shamsiddinpython215@gmail.com>
2024-08-24 01:51:24 +0500

    .

--- root/settings.py
+++ root/settings.py
@@ -95,7 +95,7 @@ DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.sqlite3',
         'NAME': BASE_DIR / 'db.sqlite3',  # TODO postgresqlga o'tqazish
-        # 'NAME': os.environ.get('DB_NAME'),w
+        # 'NAME': os.environ.get('DB_NAME'),
         # 'USER': os.environ.get('DB_USER'),
         # 'PASSWORD': os.environ.get('DB_PASSWORD'),
         # 'HOST': os.environ.get('DB_HOST'),
