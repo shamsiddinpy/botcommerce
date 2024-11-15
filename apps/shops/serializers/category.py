e70246aec3984e7ef89f8f636b52cb629729f00f
shamsiddindaminov <shamsiddinpython215@gmail.com>
2024-11-15 21:34:51 +0500

    botcommerce

--- apps/shops/serializers/category.py
+++ apps/shops/serializers/category.py
@@ -1,19 +1,14 @@
 from django.contrib.contenttypes.models import ContentType
 from rest_framework.fields import FileField
-from rest_framework.serializers import Serializer
 
 from shared.restframework.serizlaizers import DynamicFieldsModelSerializer
 from shops.models import (Category, Attachment)
 from shops.serializers.shop import AttachmentModelSerializer
 
 
-class FileUploadSerializer(Serializer):
-    file = FileField()
-
-
 class CategoryModelSerializer(DynamicFieldsModelSerializer):  # Todo Categoryani ko'rish
     attachments = AttachmentModelSerializer(many=True, read_only=True)
-    file = FileUploadSerializer(many=True, required=True)
+    file = FileField(write_only=True, required=False)
 
     class Meta:
         model = Category
@@ -22,21 +17,13 @@ class CategoryModelSerializer(DynamicFieldsModelSerializer):  # Todo Categoryani
         read_only_fields = ('show_in_ecommerce', 'status', 'shop', 'id', 'file')
 
     def create(self, validated_data):
-        attachment_data = validated_data.pop('attachments', [])
+        file = validated_data.pop('file', None)
         validated_data['status'] = Category.Status.ACTIVE
         shop_id = self.context['view'].kwargs['shop_id']
         category = Category.objects.create(shop_id=shop_id, **validated_data)
-        files = self.context.get('request').FILES.getlist('file')
-        for file in files:
-            key = f"shop/ecommerce/category/images/{category.id}/{file.name}"
-            url = f"https://fra1.digitaloceanspaces.com/botcommerce/{key}"
-            Attachment.objects.create(
-                content_type=ContentType.objects.get_for_model(Category),
-                record_id=category.id,
-                key=key,
-                url=url,
-
-            )
+        if file:
+            Attachment.objects.create(content_type=ContentType.objects.get_for_model(Category),
+                                      record_id=category.id, file=file)
         return category
 
     def get_parent(self, instance):
--- apps/shops/view/categorys.py
+++ apps/shops/view/categorys.py
@@ -1,13 +1,9 @@
-from django.contrib.contenttypes.models import ContentType
 from drf_spectacular.utils import extend_schema
 from rest_framework.filters import SearchFilter
 from rest_framework.generics import CreateAPIView
-from rest_framework.parsers import MultiPartParser, FormParser
-from rest_framework.response import Response
-from rest_framework.status import HTTP_201_CREATED
 
 from shared.restframework.paginations import PageSortNumberPagination
-from shops.models import (Category, Attachment)
+from shops.models import (Category)
 from shops.serializers.category import (CategoryModelSerializer)
 
 
@@ -16,41 +12,9 @@ class CategoryCreateAPIView(CreateAPIView):
     queryset = Category.objects.all()
     serializer_class = CategoryModelSerializer
     pagination_class = PageSortNumberPagination
-    parser_classes = [MultiPartParser, FormParser]
     filter_backends = (SearchFilter,)
     search_fields = ('name',)
 
     def get_queryset(self):
         shop_id = self.kwargs['shop_id']
         return Category.objects.filter(shop_id=shop_id)
-
-    @extend_schema(
-        request={
-            'application/json': CategoryModelSerializer,
-            'multipart/form-data': CategoryModelSerializer,  # For file uploads
-        },
-        responses={201: CategoryModelSerializer},
-    )
-    def create(self, request, *args, **kwargs):
-        files = request.FILES.getlist('file')  # Multiple files can be uploaded
-        serializer = self.get_serializer(data=request.data)
-        serializer.is_valid(raise_exception=True)
-        category = serializer.save()
-        attachment = []
-        for file in files:
-            key = f"shop/ecommerce/category/images/{category.id}/{file.name}"
-            url = f"https://fra1.digitaloceanspaces.com/botcommerce/{key}"
-            Attachment.objects.create(
-                content_type=ContentType.objects.get_for_model(Category),
-                record_id=category.id,
-                key=key,
-                url=url,
-            )
-            attachment.append({
-                'file_name': file.name,
-                'url': url
-            })
-        response_data = serializer.data
-        response_data['attachments'] = attachment  # Add file information to the response
-        headers = self.get_success_headers(serializer.data)
-        return Response(response_data, HTTP_201_CREATED, headers=headers)
