14df71583e305044a833f4b50597ada8c27621e2
shamsiddindaminov <shamsiddinpython215@gmail.com>
2024-11-19 19:15:26 +0500

    botcommerce

--- apps/shops/view/products.py
+++ apps/shops/view/products.py
@@ -1,9 +1,11 @@
 from drf_spectacular.utils import extend_schema
 from rest_framework.viewsets import ModelViewSet
 
+from shared.restframework.paginations import PageSortNumberPagination
 from shops.serializers.products import ProductModelSerializer
 
 
 @extend_schema(tags=['Products'])
 class ProductsViewSet(ModelViewSet):
     serializer_class = ProductModelSerializer
+    pagination_class = PageSortNumberPagination
