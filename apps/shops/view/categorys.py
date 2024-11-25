55b492bfbca6f689525065de72fb129db7371015
shamsiddindaminov <shamsiddinpython215@gmail.com>
2024-11-25 22:22:07 +0500

    botcommerce

--- apps/shops/view/categorys.py
+++ apps/shops/view/categorys.py
@@ -1,10 +1,10 @@
-from celery.bin.control import status
 from django.contrib.contenttypes.models import ContentType
 from django.http import FileResponse
 from drf_spectacular.utils import extend_schema
-from rest_framework.response import Response
 from rest_framework.filters import SearchFilter
-from rest_framework.generics import CreateAPIView, get_object_or_404
+from rest_framework.generics import CreateAPIView, get_object_or_404, ListCreateAPIView
+from rest_framework.response import Response
+from rest_framework.status import HTTP_404_NOT_FOUND, HTTP_500_INTERNAL_SERVER_ERROR, HTTP_204_NO_CONTENT
 from rest_framework.views import APIView
 
 from shared.restframework.paginations import PageSortNumberPagination
@@ -13,7 +13,7 @@ from shops.serializers.category import (CategoryModelSerializer)
 
 
 @extend_schema(tags=['Category'])  # category qo'shadigan API
-class CategoryCreateAPIView(CreateAPIView):
+class CategoryCreateAPIView(ListCreateAPIView, ):
     queryset = Category.objects.all()
     serializer_class = CategoryModelSerializer
     pagination_class = PageSortNumberPagination
@@ -41,19 +41,19 @@ class CategoryAttachmentDeleteAPIView(APIView):
             if attachment.file:
                 attachment.file.delete(save=False)
         except Exception as e:
-            return Response({"error": f"Error deleting file: {str(e)}"}, status=status.HTTP_500_INTERNAL_SERVER_ERROR)
+            return Response({"error": f"Error deleting file: {str(e)}"}, HTTP_500_INTERNAL_SERVER_ERROR)
         attachment.delete()
-        return Response({"detail": "Attachment successfully deleted."}, status=status.HTTP_204_NO_CONTENT)
+        return Response({"detail": "Attachment successfully deleted."}, HTTP_204_NO_CONTENT)
 
 
 @extend_schema(tags=['Category'])  # Rasimni yuklab olish (categoriyadagi rasimni yuklab olish kerak
 class DownloadCategoryImageAPIView(APIView):
 
-    def get(self, request, attachment_id):  # Todo buni ko'rish kerak rasimni yuklab olmaydpi
+    def get(self, request, image_id):  # Todo buni ko'rish kerak rasimni yuklab olmaydpi
         try:
-            attachment = Attachment.objects.get(id=attachment_id)
+            attachment = Attachment.objects.get(id=image_id)
             file_response = FileResponse(attachment.file.open(), content_type=attachment.content_type)
             file_response['Content-Disposition'] = f'attachment; filename="{attachment.file.name.split("/")[-1]}"'
-            return FileResponse(file_response)
+            return file_response
         except Attachment.DoesNotExist:
-            return Response({"error": "Image not found.."}, status=status.HTTP_404_NOT_FOUND)
+            return Response({"error": "Image not found.."}, HTTP_404_NOT_FOUND)
